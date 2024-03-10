c7dcd9df82c5c015bcf0a8910d6824392dcc675b
JTNguyen269 <148581513+JTNguyen269@users.noreply.github.com>
2024-03-09 20:55:59 -0800

    First commit
    Imports
    Fetch API information
    Randomize images

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-
-
-
 # Group Project Source Code
 #
 #
@@ -10,3 +7,26 @@
 #             https://api.imgflip.com/get_memes
 #
 # code pulls data from database and analyzes analytics
+
+
+import pandas as pd
+from sqlalchemy import create_engine, text
+import requests
+import random
+from PIL import Image
+from io import BytesIO
+import matplotlib.pyplot as plt
+import keyboard
+
+
+def fetch_images_from_api(api_url):
+    response = requests.get(api_url)
+    data = response.json()
+    memes_data = data.get('data', {}).get('memes', [])
+    return memes_data
+
+
+def select_random_image(images):
+    return random.choice(images)
+
+
