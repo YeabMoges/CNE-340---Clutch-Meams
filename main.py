787cdb527905efd79d7c7795e6ffc56d7761ab0b
Dejoo <yeabxy5@gmail.com>
2024-03-14 17:57:24 -0700

    Update Modularized

--- main.py
+++ main.py
@@ -30,45 +30,41 @@ def fetch_images_from_api(api_url):
 def select_random_image(images):
     return random.choice(images)
 
+if __name__ == "__main__":
 
-# Fetch data from the API
-api_url = "https://api.imgflip.com/get_memes"
-response = requests.get(api_url)
-data = response.json()
+    # Fetch data from the API
+    api_url = "https://api.imgflip.com/get_memes"
+    memes_data = fetch_images_from_api(api_url)
 
-# Create DataFrame from API response data
-df = pd.DataFrame(data)
+    # Create DataFrame
+    df = pd.DataFrame(memes_data)
+    # Database connection
+    db_host = '127.0.0.1'
+    db_user = 'root'
+    db_password = ''
+    db_name = 'meme'
 
-# Database connection details
-db_host = '127.0.0.1'
-db_user = 'root'
-db_password = ''
-db_name = 'meme'
+    # Create engine
+    engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")
 
-engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")
+    # Create table
+    create_table = """
+     CREATE TABLE IF NOT EXISTS memes (
+         id INT AUTO_INCREMENT PRIMARY KEY,
+         name VARCHAR(255),
+         url VARCHAR(255),
+         width INT,
+         height INT,
+         box_count INT,
+         captions INT
+     )
+     """
 
-# Define the table schema
-create_table = """
-CREATE TABLE IF NOT EXISTS memes (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(255),
-        url VARCHAR(255),
-        width INT,
-        height INT,
-        box_count INT,
-        captions INT
-)
-"""
+    # Execute the SQL command to create the table
+    with engine.connect() as connection:
+        connection.execute(text("DROP TABLE IF EXISTS memes"))
+        connection.execute(text(create_table))
 
-# Execute the SQL command to create the table
-with engine.connect() as connection:
-    connection.execute(create_table )
+        # Insert the DataFrame into the database
+        df.to_sql(name='memes', con=engine, if_exists='append', index=False)
 
-# Create SQLAlchemy engine
-engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}")
-
-# Insert the DataFrame into the database
-df.to_sql(name='gene', con=engine, if_exists='append', index=False)
-
-# Dispose the engine
-engine.dispose()
